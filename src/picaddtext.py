56b59a36cd03a64a891878e5ab14bc05206b0a4c
batalov <batalov@inpolus.ru>
2015-12-30 15:05:47 +0300

    +db

--- src/picaddtext.py
+++ src/picaddtext.py
@@ -1,20 +1,86 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-#from os import os
 from glob import glob
+import os
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
 
-"""img = Image.open("z1_p01_c1_i.jpg")
-draw = ImageDraw.Draw(img)
-# font = ImageFont.truetype(<font-file>, <font-size>)
-font = ImageFont.truetype("MyriadPro-Bold.otf", 42)
-# draw.text((x, y),"Sample Text",(r,g,b))
-draw.text((40, 35),"1",(255,255,255),font=font)
-img.save('z1_p01_c1_i-out.jpg')"""
+
+
+def read_file(filename):
+    with open(filename, 'rb') as f:
+        photo = f.read()
+    return photo
+
+
+def write_file(data, filename):
+    with open(filename, 'wb') as f:
+        f.write(data)
+        
+        
+def update_blob(id, filename):
+    data = read_file(filename)
+ 
+    query = "UPDATE Puzzles " \
+            "SET icon = %s " \
+            "WHERE puzzleid  = %s"
+ 
+    args = (data, id)
+ 
+    db_config = read_db_config()
+ 
+    try:
+        conn = MySQLConnection(**db_config)
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        conn.commit()
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def read_blob():
+    query = "SELECT puzzleid, icon FROM Puzzles"
+ 
+    # read database configuration
+    db_config = read_db_config()
+ 
+    try:
+        conn = MySQLConnection(**db_config)
+        cursor = conn.cursor()
+        cursor.execute(query)
+        for record in cursor:
+            id, icon = record
+            filename = str(id) + ".jpg"
+            write_file(icon, filename)
+ 
+    except Error as e:
+        print(e)
+ 
+    finally:
+        cursor.close()
+        conn.close()
+
+
+#read_blob()
+
+for filename in glob('*.jpg'):
+    if 'updated' in filename:
+        os.remove(filename)
+        continue
 
 for filename in glob('*.jpg'):
     img = Image.open(filename)
+#    print(filename)
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("MyriadPro-Bold.otf", 42)
-    draw.text((17, 35),"0"+filename[4:6],(0,0,0),font=font)#(255,255,255),font=font)
-    img.save(filename[4:6]+'.jpg')
+    id = str(int(filename[:-4]))
+#    print(int(id))
+    if(int(filename[:-4]) < 100): id = "0" + id
+    draw.text((17, 35), id, (0,0,0), font=font)#(255,255,255),font=font)
+    updated_filename = id + '_updated.jpg'
+    img.save(updated_filename)
+    update_blob(id, updated_filename)
